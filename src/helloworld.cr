324da4e086d8c8f3166480098f5042338d0bd14f
Jeddi212 <jedediahf51@gmail.com>
2022-06-19 22:23:59 +0700

    equaty and comparison

--- src/helloworld.cr
+++ src/helloworld.cr
@@ -1,17 +1,49 @@
 # TODO: Write documentation for `Helloworld`
 module Helloworld
-  VERSION = "0.1.0"
+	VERSION = "0.1.0"
 
-  message = "Hello World"
-  puts message
-  puts message
-  puts message
-  puts message
+	message = "Hello World"
+	puts message
+	puts message
+	puts message
+	puts message
 
-  p! typeof(message)
+	p! typeof(message)
 
-  message = 73
+	message = 73
 
-  p! typeof(message)
+	p! typeof(message)
 
+	puts
+
+	p! 1, typeof(1)
+	puts
+
+	p! 1.0, typeof(1.0)
+	puts
+
+	p! 100_000, typeof(100_000)
+	puts
+
+	p! 100_000.0, typeof(100_000.0)
+	puts
+
+	# Equaty and Comparison
+	p! 1 == 1,
+	1 == 2,
+	1.0 == 1,
+	-2000.0 == -2000
+
+	puts
+
+	p! 2 > 1,
+	1 >= 1,
+	1 < 2,
+	1 <= 2
+
+	puts
+
+	p! 1 <=> 1,
+  	2 <=> 1,
+  	1 <=> 2
 end
