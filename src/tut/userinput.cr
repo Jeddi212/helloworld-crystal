294f1bcdd19f55c940740529d6b0ef9496c9056d
Jeddi212 <jedediahf51@gmail.com>
2022-06-21 13:28:21 +0700

    user input 👤

---
+++ src/tut/userinput.cr
@@ -0,0 +1,27 @@
+print "Enter 1st value "
+n1 = gets.not_nil! # handle nil value or lack of a value
+
+print "Enter 2nc value "
+n2 = gets.not_nil!
+
+puts "#{n1} + #{n2} = #{n1.to_i + n2.to_i}"
+
+# to_i converts a string into an int
+# to_s, to_f, to_f64, to_i64, etc.
+puts "#{n1} + #{n2} = #{n1.to_i + n2.to_i}"
+puts "#{n1} - #{n2} = #{n1.to_i - n2.to_i}"
+puts "#{n1} * #{n2} = #{n1.to_i * n2.to_i}"
+puts "#{n1} / #{n2} = #{n1.to_i / n2.to_i}"
+puts "#{n1} % #{n2} = #{n1.to_i % n2.to_i}"
+puts "#{n1} ** #{n2} = #{n1.to_i64 ** n2.to_i64}"
+ 
+# Trig functions cos, sin, tan, acos, asin,
+# atan, cosh, sinh, tanh, acosh, asinh, atanh
+puts "sqrt(#{n1}) = #{Math.cbrt(n1.to_i)}"
+puts "cbrt(#{n1}) = #{Math.cbrt(n1.to_i)}"
+puts "hypot(2,2) = #{Math.hypot(2,2)}"
+puts "log(100) = #{Math.log(100)}"
+puts "log10(100) = #{Math.log10(100)}"
+puts "log2(100) = #{Math.log2(100)}"
+puts "max(100,101) = #{Math.max(100,101)}"
+puts "min(100,101) = #{Math.min(100,101)}"
