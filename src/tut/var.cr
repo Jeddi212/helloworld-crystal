d67f7659adaa124e237faa45dfaf75c6fe403a70
Jeddi212 <jedediahf51@gmail.com>
2022-06-21 12:36:44 +0700

    var 📋

---
+++ src/tut/var.cr
@@ -0,0 +1,24 @@
+puts "Hello World"
+# Comments
+
+# CONSTANTS
+PI = 3.14
+
+# Variables
+a, b = 1, 2
+c = 3
+booleans = true
+booleans_2 = false
+
+# Signed int
+puts sizeof(Int8)
+puts sizeof(Int16)
+puts sizeof(Int32)
+puts sizeof(Int64)
+
+# Unsigned int
+puts sizeof(UInt64)
+
+# Float 32 , 64
+bigFloat = 1.12345678901234
+puts (bigFloat + 0.00000000000005).to_s
